d7edc302b0606eb4216a8141030768c7e1610f77
Thomas Step <thomasstep@outlook.com>
2019-03-27 20:50:39 -0500

    Starting to make a slave. Look at README.

---
+++ slave.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import grpc
+from concurrent import futures
+import time
+import os
+import json
+import copy
+import sys
+
+# Import grpc classes
+import sns_pb2
+import sns_pb2_grpc 
+
+class snsServicer(sns_pb2_grpc.snsServiceServicer):
+
+	def __init__(self):
+		self.masterAlive = True
+		#spawn a thread 
+
+	def KeepAlive(self, request, context):
+		
+
+# Create the server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# Add class to server
+sns_pb2_grpc.add_SNSServiceServicer_to_server(snsServicer(), server)
+
+# Listen on port 8888
+print("Starting server on port 8888")
+server.add_insecure_port("[::]:" + sys.argv[1])
+server.start()
+
+try:
+	while True:
+		time.sleep(86400)
+except KeyboardInterrupt:
+	server.stop(0)
